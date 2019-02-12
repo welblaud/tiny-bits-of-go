c9012fc01ec45c65be87fede0d84d4186b1fa98d
Welblaud <honza.hejzl@gmail.com>
2019-02-12 20:47:34 +0100

    lesson 6 of unit 2 completed

--- Unit_2/Lesson_06/float.go
+++ Unit_2/Lesson_06/float.go
@@ -3,7 +3,10 @@
 
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "math"
+)
 
 func main() {
 
@@ -12,6 +15,33 @@ func main() {
 
   piggyBank := 0.1
   piggyBank += 0.2
-  fmt.Println(piggyBank)
+  fmt.Println(piggyBank) // Big inaccuracy!
+
+  fmt.Println( "22 nul:", math.Abs( piggyBank - 0.3 ) < 0.00000000000000000000001 )
+  fmt.Println( "21 nul:", math.Abs( piggyBank - 0.3 ) < 0.0000000000000000000001 )
+  fmt.Println( "20 nul:", math.Abs( piggyBank - 0.3 ) < 0.000000000000000000001 )
+  fmt.Println( "19 nul:", math.Abs( piggyBank - 0.3 ) < 0.00000000000000000001 )
+  fmt.Println( "18 nul:", math.Abs( piggyBank - 0.3 ) < 0.0000000000000000001 )
+  fmt.Println( "17 nul:", math.Abs( piggyBank - 0.3 ) < 0.000000000000000001 )
+  // The difference is comparable until the end of the inaccuracy is reached.
+  fmt.Println( "16 nul:", math.Abs( piggyBank - 0.3 ) < 0.00000000000000001 )
+  fmt.Println( "15 nul:", math.Abs( piggyBank - 0.3 ) < 0.0000000000000001 )
+  fmt.Println( "14 nul:", math.Abs( piggyBank - 0.3 ) < 0.000000000000001 )
+  fmt.Println( "13 nul:", math.Abs( piggyBank - 0.3 ) < 0.00000000000001 )
+  fmt.Println( "12 nul:", math.Abs( piggyBank - 0.3 ) < 0.0000000000001 )
+  fmt.Println( "11 nul:", math.Abs( piggyBank - 0.3 ) < 0.000000000001 )
+  fmt.Println( "10 nul:", math.Abs( piggyBank - 0.3 ) < 0.00000000001 )
+  fmt.Println( "9 nul:", math.Abs( piggyBank - 0.3 ) < 0.0000000001 )
+  fmt.Println( "8 nul:", math.Abs( piggyBank - 0.3 ) < 0.000000001 )
+  fmt.Println( "7 nul:", math.Abs( piggyBank - 0.3 ) < 0.00000001 )
+  fmt.Println( "6 nul:", math.Abs( piggyBank - 0.3 ) < 0.0000001 )
+  fmt.Println( "5 nul:", math.Abs( piggyBank - 0.3 ) < 0.000001 )
+  fmt.Println( "4 nul:", math.Abs( piggyBank - 0.3 ) < 0.00001 )
+  fmt.Println( "3 nuly:", math.Abs( piggyBank - 0.3 ) < 0.0001 )
+  fmt.Println( "2 nuly:", math.Abs( piggyBank - 0.3 ) < 0.001 )
+  fmt.Println( "1 nuly:", math.Abs( piggyBank - 0.3 ) < 0.01 )
+  fmt.Println( "žádná nula:", math.Abs( piggyBank - 0.3 ) < 0.1 )
+
+  fmt.Println( "absolutní hodnota:", math.Abs( piggyBank - 0.3 ) )
 
 }
---
+++ Unit_2/Lesson_06/savings-for-fred.go
@@ -0,0 +1,36 @@
+// savings-for-fred
+// Write a program that randomly places nickels ($0.05), dimes ($0.10),
+// and quarters ($0.25) into an empty piggy bank until it contains
+// at least $20.00. Display the running balance of the piggy bank after
+// each deposit, formatting with an appropriate width and precision.
+
+package main
+
+import (
+  
+  "fmt"
+  "math/rand"
+
+)
+
+func main() {
+
+  piggyBank := 0.0 // initial value
+
+  for piggyBank < 20.00 {
+
+ // Also possible just: switch rand.Intn(2) {
+    switch num := rand.Intn(2); num {
+    case 0:
+      piggyBank += 0.05
+    case 1:
+      piggyBank += 0.10
+    case 2:
+      piggyBank += 0.25
+    }
+
+    fmt.Printf("In the piggy is: $%5.2f\n", piggyBank)
+
+  }
+
+}
