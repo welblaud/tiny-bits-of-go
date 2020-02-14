d141f6ad5c7d59b749871e33b679346dc4421bd7
Honza Hejzl <honza.hejzl@gmail.com>
2020-02-14 16:22:51 +0100

    Formatting.

--- Unit_1/Lesson_03/cave.go
+++ Unit_1/Lesson_03/cave.go
@@ -1,18 +1,19 @@
 // cave.go
 package main
+
 import "fmt"
 
 func main() {
 
-  var point = "entrance"
+	var point = "entrance"
 
-  if point == "cave" {
-    fmt.Println("You are inside a dark cave.")
-  } else if point == "entrance" {
-    fmt.Println("You are before a dark entrance. It probably leads to a much darker cave.")
-  } else if point == "mountain" {
-    fmt.Println("You are standing on the top of the mountain of the Dead.")
-  } else {
-    fmt.Println("What the hell are you standing in front of?!")
-  }
+	if point == "cave" {
+		fmt.Println("You are inside a dark cave.")
+	} else if point == "entrance" {
+		fmt.Println("You are before a dark entrance. It probably leads to a much darker cave.")
+	} else if point == "mountain" {
+		fmt.Println("You are standing on the top of the mountain of the Dead.")
+	} else {
+		fmt.Println("What the hell are you standing in front of?!")
+	}
 }
