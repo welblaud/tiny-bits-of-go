7d2ac3f8b6d7bf2642bf8610d9be07c98742b1bb
Honza Hejzl <honza.hejzl@gmail.com>
2020-02-25 16:34:53 +0100

    minor  bufixes

--- Unit_1/Lesson_02/fmt.go
+++ Unit_1/Lesson_02/fmt.go
@@ -5,13 +5,13 @@ package main
 import "fmt"
 
 func main() {
-  // It is possible to pass several arguments to the Print and Println functions, they will concatenated.
-  // It is also possible to use format verbs (e.g., %v) in Printf function.
-  fmt.Print("My weight on the surface of Mars is", 72 * 0.3783, "kilograms, and I would be", 38 * 365.2425 / 687, "years old.\n")
-  fmt.Println("My weight on the surface of Mars is", 72 * 0.3783, "kilograms, and I would be", 38 * 365.2425 / 687, "years old.")
-  fmt.Printf("My weight on the surface of Mars is %v kilograms,", 72 * 0.3783) // Like Print, but with format verbs.
-  fmt.Printf(" and I would be %v years old.\n", 38 * 365/687)
-  fmt.Printf("My weight on the surface of %v is %v kilograms.\n", "Earth", 72) // Multiple format verbs.
-  fmt.Printf("%-15v $%4v\n", "SpaceX", 94) // Aligning text. Negative value pads the value to left, positive to right.
-  fmt.Printf("%-15v $%4v\n", "Virgin Galactic", 100)
+	// It is possible to pass several arguments to the Print and Println functions, they will concatenated.
+	// It is also possible to use format verbs (e.g., %v) in Printf function.
+	fmt.Print("My weight on the surface of Mars is ", 72*0.3783, "kilograms, and I would be ", 38*365.2425/687, " years old.\n")
+	fmt.Println("My weight on the surface of Mars is", 72*0.3783, "kilograms, and I would be ", 38*365.2425/687, "years old.")
+	fmt.Printf("My weight on the surface of Mars is %v kilograms,", 72*0.3783) // Like Print, but with format verbs.
+	fmt.Printf(" and I would be %v years old.\n", 38*365/687)
+	fmt.Printf("My weight on the surface of %v is %v kilograms.\n", "Earth", 72) // Multiple format verbs.
+	fmt.Printf("%-15v $%4v\n", "SpaceX", 94)                                     // Aligning text. Negative value pads the value to left, positive to right.
+	fmt.Printf("%-15v $%4v\n", "Virgin Galactic", 100)
 }
--- Unit_1/Lesson_02/malacandra.go
+++ Unit_1/Lesson_02/malacandra.go
@@ -5,10 +5,10 @@ package main
 import "fmt"
 
 func main() {
-  const days = 28
-  var distance = 56000000
-  var speed = 56000000 / 28 / 24
-  // or 56000000/(28*24)
+	const days = 28
+	var distance = 56000000
+	var speed = 56000000 / 28 / 24
+	// or 56000000/(28*24)
 
-  fmt.Printf("For the given distance of %v km, the ship has to travel %v km/h for reaching Mars in %v days.", distance, speed, days)
+	fmt.Printf("For the given distance of %v km, the ship has to travel %v km/h for reaching Mars in %v days.", distance, speed, days)
 }
