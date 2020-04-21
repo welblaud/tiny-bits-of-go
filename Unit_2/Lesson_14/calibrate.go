988b73e393204bf9c1b447d336960116be282c51
welblaud <honza.hejzl@gmail.com>
2020-04-21 17:39:36 +0200

    more functionality added

--- Unit_2/Lesson_14/calibrate.go
+++ Unit_2/Lesson_14/calibrate.go
@@ -1,17 +1,27 @@
 // calibrate.go
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 type kelvin float64
 
 // sensor function type
 type sensor func() kelvin
 
+// ready for plugging in a real sensor
 func realSensor() kelvin {
 	return 0
 }
 
+// fakeSensor generates random temperature on Mars
+func fakeSensor() kelvin {
+	return kelvin(rand.Intn(151) + 150)
+}
+
+// calibrate improves a sensor functionality
 func calibrate(s sensor, offset kelvin) sensor {
 	return func() kelvin {
 		return s() + offset
@@ -19,8 +29,12 @@ func calibrate(s sensor, offset kelvin) sensor {
 }
 
 func main() {
-	sensor := calibrate(realSensor, 5)
-	fmt.Println(sensor())
-	sensor = calibrate(realSensor, 8)
-	fmt.Println(sensor())
+	var offset kelvin = 4
+	sensor1 := calibrate(realSensor, offset)
+	fmt.Println(sensor1())
+	sensor2 := calibrate(fakeSensor, offset)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(sensor2())
+	}
 }
